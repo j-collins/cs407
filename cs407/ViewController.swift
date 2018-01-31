6fb8646587cc1d43cb2df8c1dfe4a66c7ce353ad
j-collins <colli206@purdue.edu>
2018-01-30 19:14:45 -0500

    Initial Commit

---
+++ cs407/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cs407
+//
+//  Created by Johanna Collins on 1/30/18.
+//  Copyright © 2018 CS407Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
