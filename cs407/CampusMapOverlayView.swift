19045f158d79c06e26939eee50d305a205c127e3
ajnussel <anussel@purdue.edu>
2018-02-14 22:16:10 -0500

    CampusMapOverlayView.swift file needed for map overlay

---
+++ cs407/CampusMapOverlayView.swift
@@ -0,0 +1,29 @@
+//
+//  CampusMapOverlayView.swift
+//  cs407
+//
+//  Created by Ashley Nussel on 2/14/18.
+//  Copyright © 2018 CS407Group. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class CampusMapOverlayView: MKOverlayRenderer {
+    var overlayImage: UIImage
+    
+    init(overlay:MKOverlay, overlayImage:UIImage) {
+        self.overlayImage = overlayImage
+        super.init(overlay: overlay)
+    }
+    
+    override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
+        guard let imageReference = overlayImage.cgImage else { return }
+        
+        let rect = self.rect(for: overlay.boundingMapRect)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.translateBy(x: 0.0, y: -rect.size.height)
+        context.draw(imageReference, in: rect)
+    }
+}
