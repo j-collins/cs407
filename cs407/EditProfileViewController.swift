74c6517f7b61fefa2c17848af29a86bca9450ad4
piyushi jaiswal <>
2018-04-12 22:13:50 -0400

    profile ui

---
+++ cs407/EditProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditProfileViewController.swift
+//  cs407
+//
+//  Created by piyushi jaiswal on 4/12/18.
+//  Copyright © 2018 CS407Group. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
+
+class EditProfileViewController: UIViewController {
+}
---
+++ cs407/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  cs407
+//
+//  Created by piyushi jaiswal on 4/12/18.
+//  Copyright © 2018 CS407Group. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
+
+class ProfileViewController: UIViewController {
+}
