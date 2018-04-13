b386f11e8facba92da7eda94adfd0b69d298acf6
piyushi jaiswal <>
2018-04-13 01:37:22 -0400

    implemeted buttons and display for profile

--- cs407/EditProfileViewController.swift
+++ cs407/EditProfileViewController.swift
@@ -13,4 +13,19 @@ import FirebaseDatabase
 import FirebaseStorage
 
 class EditProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func ActLogout(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+            self.present(vc!, animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+    
+
 }
--- cs407/ProfileViewController.swift
+++ cs407/ProfileViewController.swift
@@ -13,4 +13,26 @@ import FirebaseDatabase
 import FirebaseStorage
 
 class ProfileViewController: UIViewController {
+    @IBOutlet weak var LabelEmail: UILabel!
+    @IBOutlet var LabelPassword: UILabel!
+    @IBOutlet var LabelName: UILabel!
+    let user = Auth.auth().currentUser?.email
+    //LabelEmail.text = user
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.LabelEmail.text = user;
+        self.LabelPassword.text = "********"
+    }
+ 
+    @IBAction func LogoutAction(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+            self.present(vc!, animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+    
 }
